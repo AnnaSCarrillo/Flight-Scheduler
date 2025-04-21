f44fc2d8c29dead52d095aa831620c169119e47a
AnnaSCarrillo <annasofiacarrillo98@gmail.com>
2025-04-21 10:18:34 -0400

    Wrote Time class, will test later

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <sstream>
 #include <algorithm>
 
 using namespace std;
@@ -29,14 +30,48 @@ class TimeBlocks {
 
 class Time {
     public:
+        Time(string TimeString) {
+            SetHourMinutes(TimeString);
+            totalMinutes = (hour * 60) + minutes;
+        }
+
+        void SetHourMinutes(const string& TimeString) {
+            hour = stoi(TimeString.substr(0, 2));
+            minutes = stoi(TimeString.substr(2, 2));
+        }
+
+        string ToString() {
+            int h = hour;
+            int m = minutes;
+            stringstream ss;
+            if (h < 10) ss << "0";
+            ss << h;
+            if (m < 10) ss << "0";
+            ss << m;
+
+            return ss.str();
+        }
+
+        void AdjustTime(int delta) {
+            totalMinutes += delta;
+
+            totalMinutes = (totalMinutes + 1440) % 1440;
+            hour = totalMinutes / 60;
+            minutes = totalMinutes % 60;
+        }
     private:
+        int hour;
+        int minutes;
+        int totalMinutes;
 };
 
 int main() {
 
     FlightTimes test("0630", true, "Boston");
 
-    cout << test.GetTime() << " " << test.GetDeparture() << " " << test.GetLocation() << endl;
+    Time testTime(test.GetTime());
+
+    cout << testTime.GetHour() << " " << testTime.GetMinutes() << endl;
 
     return 0;
 }
